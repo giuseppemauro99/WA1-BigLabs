c208c51ee60d95353d9ec15c099dbb8ed38fb0d8
giuseppemauro99 <giuseppemauro99@users.noreply.github.com>
2022-04-10 16:18:25 +0200

    week1 - biglab1A

--- biglab1/src/App.js
+++ biglab1/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {Table,Col,Row} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import dayjs from 'dayjs';
+import {Film, FilmLibrary} from "./FilmLibrary.js";
+
+const FakeFilmList = [
+  new Film(1,"Pulp Fiction", true, dayjs("March 10, 2022"), 5),
+  new Film(2,"21 Grams", true, dayjs("March 17, 2022"), 4),
+  new Film(3,"Star Wars"),
+  new Film(4,"Matrix"),
+  new Film(5,"Shrek", false, dayjs("March 21, 2022"), 3)
+]
+
+function PageTitle(props){
+  return(
+    <Col>
+      <h1>My Fake Film List</h1>
+    </Col>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table>
+      <Row>
+        <PageTitle />
+      </Row>
+    </Table>
   );
 }
 
---
+++ biglab1/src/FilmLibrary.js
@@ -0,0 +1,29 @@
+function Film(id, title, isFavourite = false, watchDate, rating){
+    this.id = id;
+    this.title = title;
+    this.isFavourite = isFavourite;
+    this.watchDate = watchDate;
+    this.rating = rating;
+}
+
+function FilmLibrary(){
+    this.films = [];
+    this.addNewFilm = (film) => this.films.push(film);
+
+    this.sortByDate = () => {
+        // [...variableName] crea un nuovo array (di default userebbe il riferimento a quello vecchio)
+        return [...this.films].sort((a,b) => {
+                if(a.watchDate == undefined) return 1; //mette alla fine gli undefined
+                if(b.watchDate == undefined) return -1;
+                a.watchDate.isAfter(b.dateWatched) ? 1 : -1;
+                });
+    }
+
+    this.deleteFilm = (film) => this.films = this.films.filter( a => a != film);
+
+    this.getRated = () => {
+        return [...this.films].filter(a => a.rating != undefined).sort( (a,b) => a.rating > b.rating ? -1 : 1);
+    }
+}
+
+export {Film, FilmLibrary};
